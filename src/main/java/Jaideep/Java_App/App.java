508c8b83fefe832c6ec5e55af6a440575c1a125d
jaideep413 <maruvadajaideep@gmail.com>
2023-12-28 14:30:37 +0530

    maven java push

---
+++ src/main/java/Jaideep/Java_App/App.java
@@ -0,0 +1,13 @@
+package Jaideep.Java_App;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
